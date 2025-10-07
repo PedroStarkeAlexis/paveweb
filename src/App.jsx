ca1dd81660327a546b3644a4279cbc06aa5d0cc0
PedroAlexis <64668385+pedro730@users.noreply.github.com>
2025-10-07 13:49:24 -0300

    Comenta o link de navegação para a página inicial no componente App

--- src/App.jsx
+++ src/App.jsx
@@ -260,7 +260,7 @@ function App() {
                 </div>
                 <nav className="sidebar-nav">
                     <ul>
-                        <NavLink to="/" icon={IconHome}>Início</NavLink>
+                        {/* <NavLink to="/" icon={IconHome}>Início</NavLink> */}
                         <NavLink to="/calculadora" icon={IconCalculator}>Calculadora PAVE</NavLink>
                         <NavLink to="/banco-questoes" icon={IconBook}>Banco de Questões</NavLink>
                         <NavLink to="/chat" icon={IconChat}>Assistente IA</NavLink>
